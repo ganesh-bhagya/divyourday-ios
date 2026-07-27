ff530c66b27b6c2aea75ee2fc371949ff977d73e
ganeshwanigarathne <ganesh.wanigarathne@ideahub.lk>
2026-07-27 09:20:19 +0530

    Initial Commit

---
+++ Divyourday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Divyourday
+//
+//  Created by Ganesh Bhagya Sampath Wanigarathne on 2026-07-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Divyourday/DivyourdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivyourdayApp.swift
+//  Divyourday
+//
+//  Created by Ganesh Bhagya Sampath Wanigarathne on 2026-07-27.
+//
+
+import SwiftUI
+
+@main
+struct DivyourdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
